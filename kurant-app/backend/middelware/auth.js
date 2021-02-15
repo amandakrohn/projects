6a4e83bf3ddce6ccce483ed6400f775399776f91
Amanda Krohn <akrohn@kth.se>
2021-02-15 17:55:02 +0100

    compatible w cookies instead of local storage

--- kurant-app/backend/middelware/auth.js
+++ kurant-app/backend/middelware/auth.js
@@ -2,18 +2,17 @@ const jwt = require('jsonwebtoken')
 
 function auth(req, res, next){
     try{
-        const token = req.header('x-auth-token')
-        if(!token)
-            return res.status(401).json({msg: "No auth token. Authorization denied."})
+        const token = req.cookies.token
+        if(!token) return res.status(401).json({msg: "No auth token found. Unauthorized."})
     
         const verified = jwt.verify(token, process.env.JWT_SECRET);
-        if(!verified)
-            return res.status(401).json({msg: "Token verification failed, authorization failed."})
+        if(!verified) return res.status(401).json({msg: "Token verification failed, authorization failed."})
 
         req.user = verified.id;
         next()
     } catch(err){
-        res.status(500).json({msg: err.message})
+        console.error(err)
+        res.status(401).json({msg: "Unauthorized"})
     }
 }
 
